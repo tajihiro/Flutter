e2f64fe16f083e8c648f7803fc9bc9624fb7eedc
Hiroyuki Tajima <tajima@blowfish.co.jp>
2020-05-13 22:07:20 +0900

    Create Weather Application.

---
+++ sample/bitcoin_app/android/app/src/main/kotlin/jp/co/blowfish/bitcoinapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.co.blowfish.bitcoinapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sample/bitcoin_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bitcoinapp/price_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.indigoAccent,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
